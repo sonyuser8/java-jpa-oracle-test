e9a7b37dc02b176a163ddb57e8781f7c1b8fb80b
staran0501 <sonyuser8@yahoo.com.tw>
2020-01-10 00:12:59 +0800

    no message

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -7,6 +7,7 @@ import javax.sql.DataSource;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 
 import oracle.jdbc.pool.OracleDataSource;
@@ -18,14 +19,22 @@ public class DemoApplication {
 		SpringApplication.run(DemoApplication.class, args);
 	}
 	
+//	@Bean
+//    DataSource dataSource() throws SQLException {
+//        OracleDataSource dataSource = new OracleDataSource();
+//        dataSource.setUser("sys as sysdba");
+//        dataSource.setPassword("1qaz2wsx");
+//        dataSource.setURL("jdbc:oracle:thin:@localhost:1521:orcl");
+//        dataSource.setImplicitCachingEnabled(true);
+//        dataSource.setFastConnectionFailoverEnabled(true);
+//        return dataSource;
+//	}
 	@Bean
-    DataSource dataSource() throws SQLException {
-        OracleDataSource dataSource = new OracleDataSource();
-        dataSource.setUser("sys as sysdba");
-        dataSource.setPassword("QQ");
-        dataSource.setURL("jdbc:oracle:thin:@localhost:1521:orcl");
-        dataSource.setImplicitCachingEnabled(true);
-        dataSource.setFastConnectionFailoverEnabled(true);
-        return dataSource;
-	}
+    public DataSource getDataSource() {
+        DataSourceBuilder dataSourceBuilder = DataSourceBuilder.create();
+        dataSourceBuilder.url("jdbc:oracle:thin:@localhost:1521:orcl");
+        dataSourceBuilder.username("sys as sysdba");
+        dataSourceBuilder.password("1qaz2wsx");
+        return dataSourceBuilder.build();
+    }
 }
